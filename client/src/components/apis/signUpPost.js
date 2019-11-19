f214f95a7cd7eda2166a3113c219adaf627b5e36
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-19 10:53:05 -0800

    home page data split

--- client/src/components/apis/signUpPost.js
+++ client/src/components/apis/signUpPost.js
@@ -1,23 +1,25 @@
 import axiosWithAuth from 'axios'
-
-// const signUpPost = values => {
-//   axios.post('https://reqres.in/api/users', values)
-//   .then(res => {
-//     console.log(res)
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
-// }
+import axios from "axios"
 
 const signUpPost = values => {
-  axiosWithAuth()
-    .post('/auth/register', values)
-    .then((res) => {
-      localStorage.setItem('token', res.data.token);
-      // localStorage.setItem('user', res.data.id);
-    })
-    .catch((err) => console.log(err));
-  };
+  axios.post('https://reqres.in/api/users', values)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+// const signUpPost = values => {
+//   axiosWithAuth()
+//     .post('/auth/register', values)
+//     .then((res) => {
+//       console.log(res)})
+    //   localStorage.setItem('token', res.data.token);
+    //   // localStorage.setItem('user', res.data.id);
+    // })
+  //   .catch((err) => console.log(err));
+  // };
 
 export default signUpPost
--- client/src/components/homePage/HomePage.js
+++ client/src/components/homePage/HomePage.js
@@ -1,26 +1,30 @@
 import React, { useContext } from 'react'
 import { UserTabsContext } from '../../UserTabsContext'
-import { H3, P } from '../../EmoTools'
+import { H3, P, FlexContainer } from '../../EmoTools'
+import { sortedTabs } from './HomeData'
 
 const HomePage = () => {
-  const {userTabArray, setUserTabArray} = useContext(UserTabsContext)
-
-  console.log(userTabArray)
-
-  const sortedTabs = Array.from(
-    new Set(userTabArray.tabs.map(tab => tab.category_id))
+  const { userTabArray } = useContext(UserTabsContext)
+  console.log(sortedTabs(userTabArray))
+  return (
+    <>
+      {sortedTabs(userTabArray).map((tabObject, i) => (
+        <FlexContainer 
+        key={i}
+        mainColor
+        fdc
+        aic
+        acc
+        margin='20px'
+        >
+          <H3>{tabObject.catName}</H3>
+          {tabObject.tabName.map((tab, i) => (
+            <P key={i}>{tab}</P>
+          ))}
+        </FlexContainer>
+      ))}
+    </>
   )
-    .map(catID => userTabArray.tabs.filter(tab => catID === tab.category_id))
-    .map(array => (
-      <>
-        <H3>{array[0].category}</H3>
-        {array.map(tab => (
-          <P>{tab.name}</P>
-        ))}
-      </>
-    ))
-
-  return <>{sortedTabs}</>
 }
 
 export default HomePage
