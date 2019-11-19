6cff303c4698d82b1718e4cbb21c2e343d750d1b
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-18 21:30:50 -0800

    login working

--- client/src/components/apis/loginPost.js
+++ client/src/components/apis/loginPost.js
@@ -1,13 +1,22 @@
 import axios from 'axios'
+import axiosWithAuth from "../utils/axiosWithAuth"
+
+// const loginPost = values => {
+//   axios.post('https://reqres.in/api/users', values)
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }
 
 const loginPost = values => {
-  axios.post('https://reqres.in/api/users', values)
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
-
-export default loginPost
+  axiosWithAuth()
+    .post('/auth/login', values)
+    .then((res) => {
+      localStorage.setItem('token', res.data.token);
+    })
+    .catch((err) => console.log(err));
+  };
+export default loginPost;
--- client/src/components/apis/signUpPost.js
+++ client/src/components/apis/signUpPost.js
@@ -1,13 +1,23 @@
-import axios from 'axios'
+import axiosWithAuth from 'axios'
+
+// const signUpPost = values => {
+//   axios.post('https://reqres.in/api/users', values)
+//   .then(res => {
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }
 
 const signUpPost = values => {
-  axios.post('https://reqres.in/api/users', values)
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
+  axiosWithAuth()
+    .post('/auth/register', values)
+    .then((res) => {
+      localStorage.setItem('token', res.data.token);
+      // localStorage.setItem('user', res.data.id);
+    })
+    .catch((err) => console.log(err));
+  };
 
 export default signUpPost
