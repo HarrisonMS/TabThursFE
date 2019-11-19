4be7e6f271a7339eb95d330dbc5d0181dab22b30
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-19 13:21:28 -0800

    Login and signup somewhat functional commented out code showing on signup

--- client/src/components/apis/signUpPost.js
+++ client/src/components/apis/signUpPost.js
@@ -1,25 +1,24 @@
-import axiosWithAuth from 'axios'
+import axiosWithAuth from "../utils/axiosWithAuth"
 import axios from "axios"
 
-const signUpPost = values => {
-  axios.post('https://reqres.in/api/users', values)
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
-
 // const signUpPost = values => {
-//   axiosWithAuth()
-//     .post('/auth/register', values)
-//     .then((res) => {
-//       console.log(res)})
-    //   localStorage.setItem('token', res.data.token);
-    //   // localStorage.setItem('user', res.data.id);
-    // })
-  //   .catch((err) => console.log(err));
-  // };
+//   axios.post('https://reqres.in/api/users', values)
+//   .then(res => {
+//     // console.log(res)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+// }
+
+const signUpPost = values => {
+  axiosWithAuth()
+    .post('/auth/register', values)
+    .then((res) => {
+      localStorage.setItem('token', res.data.token);
+      props.history.push("/home")
+    })
+    .catch((err) => console.log(err));
+  };
 
 export default signUpPost
