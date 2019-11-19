39958a0ba5c007bd1b294f86ddc7bfa87acc96b7
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-18 18:07:29 -0800

    added useContext hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState }from 'react'
 import NavBar from './components/navigation/NavBar'
 import EmoGlobal from './EmoGlobal'
+import { UserTabsContext } from './UserTabsContext'
 
 function App() {
+  const [userTabArray, setUserTabArray] = useState({
+    tabs: [
+      {
+        id: 1,
+        url: 'https://google.com/',
+        name: 'googly',
+        notes: 'find stuff',
+        user_id: 1,
+        category_id: 1,
+        category: 'misc',
+      },
+      {
+        id: 2,
+        url: 'https://youtube.com/',
+        name: 'youtube-y',
+        notes: 'watch stuff',
+        user_id: 1,
+        category_id: 2,
+        category: 'media',
+      },
+      {
+        id: 3,
+        url: 'https://bing.com/',
+        name: 'bingy',
+        notes: 'find stuff',
+        user_id: 1,
+        category_id: 1,
+        category: 'misc',
+      },
+    ],
+  })
   return (
     <>
       <EmoGlobal />
+    <UserTabsContext.Provider value={{userTabArray, setUserTabArray}}>
       <NavBar />
+      </UserTabsContext.Provider>
     </>
   )
 }
