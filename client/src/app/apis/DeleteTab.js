3aca2ebd514cec7a738d207ae766db138fefdbda
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-20 11:24:34 -0800

    added crud operations in apis folder

--- client/src/app/apis/DeleteTab.js
+++ client/src/app/apis/DeleteTab.js
@@ -3,7 +3,7 @@ import axiosWithAuth from "../utils/axiosWithAuth";
 export const deleteTab = tab => {
   console.log(tab);
   axiosWithAuth()
-    .delete(`${tab.id}`)
+    .delete(`/tabs/${id}/${tab.id}`)
     .then(res => {
       console.log(res);
     })
