c998435e2ee85745c109b51586cf7552fa2c45b0
michelangelo17 <michelangelo.markus@gmail.com>
2019-11-19 19:32:13 -0800

    clean up

--- client/src/components/tabCategory/TabCategory.js
+++ client/src/components/tabCategory/TabCategory.js
@@ -1,26 +1,25 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { H1, FlexContainer, H2, P } from '../../EmoTools'
 import { UserTabsContext } from '../../UserTabsContext'
 import MicroLinkPreviews from '../utils/MicroLinkPreviews'
 
 const TabCategory = props => {
   const { userTabArray } = useContext(UserTabsContext)
-  const categoryTabs = userTabArray.tabs.filter(tab => tab.category === props.match.params.id)
-  console.log(categoryTabs[0].url)
+  const categoryTabs = userTabArray.tabs.filter(
+    tab => tab.category === props.match.params.id
+  )
   return (
     <>
-    <H1>{props.match.params.id}</H1>
-    {categoryTabs.map(tab => (
-      <FlexContainer margin='20px auto' width='498px'>
-      <FlexContainer fdc>
-      <H2 margin='20px 10px 10px'>{tab.name}</H2>
-      <P margin='10px'>{tab.notes}</P>
-      </FlexContainer>
-      <MicroLinkPreviews address={tab.url} />
-      </FlexContainer>
-    ))}
-    
-    
+      <H1>{props.match.params.id}</H1>
+      {categoryTabs.map(tab => (
+        <FlexContainer margin='20px auto' width='498px'>
+          <FlexContainer fdc>
+            <H2 margin='20px 10px 10px'>{tab.name}</H2>
+            <P margin='10px'>{tab.notes}</P>
+          </FlexContainer>
+          <MicroLinkPreviews address={tab.url} />
+        </FlexContainer>
+      ))}
     </>
   )
 }
